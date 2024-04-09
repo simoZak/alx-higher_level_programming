59edfe753e53802148bee27f891ee783d9c66623
simoZak <zakhnini92@gmail.com>
2024-04-09 17:30:16 +0000

    101-locked_class.py task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-class LockedClass:
+class LockedClass():
         """
         Prevent the user from instantiating new LockedClass attributes
         for anything but attributes called 'first_name'.
