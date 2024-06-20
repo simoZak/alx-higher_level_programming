c67055abe8f9f8d19c7364a75512c35e3b6d430c
simoZak <zakhnini92@gmail.com>
2024-06-20 21:51:49 +0100

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,19 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-  let nOccurrences = 0;
-  for (let i = 0; i < list.length; i++) {
-    if (searchElement === list[i]) {
-      nOccurrences++;
+const SquareP = require('./5-square');
+
+class Square extends SquareP {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
     }
   }
-  return nOccurrences;
-};
+}
+
+module.exports = Square;
